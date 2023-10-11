0e4239d8ea71f8fd7b3090c29d300fc6746cb1fb
Justin <justinfisher340@gmail.com>
2023-10-11 00:05:44 -0400

    project commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [movie,setMovie]=useState("")
+
+  const handleChange=(e)=>{
+    setMovie(e.target.value);
+  }
+
+  function retrievePoster(){
+    const curPoster=document.getElementById("curPoster");
+    const key=process.env.REACT_APP_KEY;
+  
+    let m=movie.trim();
+    const url=`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${m}`;
+    
+    fetch(url).then(res=>res.json()).then(data=>{
+      if(data.results){
+        const path=data.results[0]['poster_path'];
+        const newPath=`https://image.tmdb.org/t/p/w200${path}`;
+        curPoster.src=newPath;
+      }
+    })
+  
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+      <h1>Movie Poster Display</h1>
+      <input type="text" id="movieInput" placeholder="Enter a movie title" value={movie} onChange={handleChange}></input>
+      <button id="searchButton" onClick={retrievePoster} class='btn btn-primary'>Search</button>
+
+      <div id="posterContainer">
+        <img id="curPoster"></img>
+      </div>
+          
     </div>
   );
 }
